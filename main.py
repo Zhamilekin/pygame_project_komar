3b37335d686386aec73beb16b94a728215b546b0
Zhamilya <94537577+Zhamilekin@users.noreply.github.com>
2022-01-24 21:59:42 +0600

    Update main.py
    requirements.txt, подсчет очков, файл для подсчета очков

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ clock = pygame.time.Clock()
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join(name)
+    fullname = os.path.join('data', name)
     image = pygame.image.load(fullname)
     if colorkey is not None:
         image = image.convert()
@@ -33,7 +33,7 @@ class Islands(pygame.sprite.Sprite):
     def __init__(self, x, hgt, *group):
         super().__init__(platforms)
         self.image = load_image('baget.png')
-        # self.image = pygame.transform.scale(self.image, (205, 30))
+        self.image = pygame.transform.scale(self.image, (205, 30))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = hgt
@@ -46,9 +46,9 @@ class Islands(pygame.sprite.Sprite):
 
 class Fumigator(pygame.sprite.Sprite):
     def __init__(self, x, hgt, *group):
-        super().__init__(platforms)
-        self.image = load_image('fumigator.jpg', -1)
-        self.image = pygame.transform.scale(self.image, (75, 35))
+        super().__init__(fumigator)
+        self.image = load_image('fumigator.png')
+        self.image = pygame.transform.scale(self.image, (70, 70))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = hgt
@@ -68,8 +68,9 @@ class Komar(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         # self.mask = pygame.mask.from_surface(self.image)
         self.velocity = 0
-        self.rect.center = (350, 300)
+        self.rect.center = (280, 300)
         self.side = False
+        self.score = 0
 
     def play(self, frst):
         up = 0
@@ -103,15 +104,14 @@ class Komar(pygame.sprite.Sprite):
                     self.rect.bottom = collision_plat[0].rect.top
                     dy = 0
                     self.velocity = -20
+                    self.score += 1
         if pygame.sprite.spritecollideany(self, fumigator):
             self.kill()
+
         if frst:
             if self.rect.bottom + dy > 600:
                 dy = 0
                 self.velocity = -20
-            # else:
-            #     dy = 0
-            #     self.ve
 
         if self.rect.top <= 200:
             if self.velocity < 0:
@@ -125,8 +125,6 @@ class Komar(pygame.sprite.Sprite):
 
         return up
 
-
-switch = True
 komar = Komar()
 all_sprites.add(komar)
 
@@ -137,10 +135,10 @@ class GameStates():
         self.start = True
         self.limit = 15
         self.switch = True
-        self.h = 400
-        self.island = Islands(222, 440)
-        self.fumigator = Fumigator(222, 440)
+        self.h = 350
+        self.island = Islands(200, 440)
         platforms.add(self.island)
+        self.fumigator = Fumigator(-40, 250)
         self.num_y1 = 150
 
     def intro(self):
@@ -154,6 +152,7 @@ class GameStates():
         pygame.display.flip()
 
     def main_game(self):
+        userInput = pygame.key.get_pressed()
         clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -161,20 +160,40 @@ class GameStates():
                 sys.exit()
             if event.type == pygame.KEYDOWN:
                 self.start = False
+            if userInput[pygame.K_RCTRL] or userInput[pygame.K_LCTRL]:
+                f = open("scores.txt", "r")
+                a = f.readlines()
+                if len(a) == 0 or (len(a) == 1 and a[0] == "\n"):
+                    result = '1 result:' + str(komar.score)
+                    f = open("scores.txt", "a")
+                    f.write(result + '\n')
+                    f.close()
+                else:
+                    f = open("scores.txt", "r")
+                    num = f.readlines()
+                    f = open("scores.txt", "a")
+                    cnt = int(num[-1][0]) + 1
+                    result = str(cnt) + ' result:' + str(komar.score)
+                    f.write(result + '\n')
+                    f.close()
         up = komar.play(self.start)
         if len(all_sprites) == 1:
             screen.blit(background, (0, 0))
             all_sprites.draw(screen)
             platforms.draw(screen)
+            fumigator.draw(screen)
+            if len(platforms) < self.limit:
+                self.island = Islands(random.randint(-100, 350), self.island.rect.y - random.randint(140, 155))
+                platforms.add(self.island)
+                self.fumigator = Fumigator(random.randint(-100, 650), self.fumigator.rect.y - 300)
+                fumigator.add(self.fumigator)
+                self.num_y1 -= 150
+            platforms.update(up)
+            fumigator.update(up)
         else:
-            end_screen()
-        if len(platforms) < self.limit:
-            self.island = Islands(random.randint(-100, 350), self.island.rect.y - random.randint(140, 155))
-            platforms.add(self.island)
-            self.fumigator = Fumigator(random.randint(-100, 650), self.fumigator.rect.y - 300)
-            fumigator.add(self.fumigator)
-            self.num_y1 -= 150
-        platforms.update(up)
+            komar.kill()
+            platforms.empty()
+            end_screen(komar.score)
         pygame.display.flip()
 
     def state_image(self):
@@ -206,13 +225,14 @@ def start_screen():
         screen.blit(text_welcome, intro_rect)
 
 
-def end_screen():
+def end_screen(final_score):
     global gr, x, y
+    global game_score
     screen.fill((0, 0, 0))
-    intro_text = ["score: ", "",
-                  "from zhamilya and dameli", "", "", "", "",
+    intro_text = ["score: ", str(final_score),
+                  "from Zhamilya and Dameli", "", "", "", "",
                   "", "", "", "", "",
-                  "thanks!"]
+                  "thanks!", "", "Для записи результата нажмите CTRL"]
     fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
     screen.blit(pygame.transform.scale(fon, [200, 200]), (150, 150))
     font = pygame.font.Font(None, 30)
@@ -220,10 +240,10 @@ def end_screen():
     for i in intro_text:
         text_welcome = font.render(i, True, (255, 255, 255))
         intro_rect = text_welcome.get_rect()
-        intro_rect.center = (500, 00)
+        intro_rect.center = (500, 0)
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 170
+        intro_rect.x = 150
         text_coord += intro_rect.height
         screen.blit(text_welcome, intro_rect)
 
@@ -233,3 +253,4 @@ def end_screen():
 start_screen()
 while True:
     game_state.state_image()
+
